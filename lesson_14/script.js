6fc33214f3c15a3c0157efdb8b30025ea8e57a5e
Leonid Zychkov <buldog52@gmail.com>
2018-10-28 23:45:19 +0200

    Finished tasks with forms in hw14. Added method validateForm to node.js

--- lesson_14/script.js
+++ lesson_14/script.js
@@ -1,4 +1,89 @@
-﻿(function(){
+﻿console.log('Домашняя работа N14. Работа с формами. ');
+console.log('Задачи на стр 9-11.');
+
+(function() {
+
+  console.log('1. Создать форму для добавления юзера в базу данных (пока без базы данных:-)).  ' +
+    'Должны быть следующие поля: имя, фамилия, возраст, электронный адрес.  При сабмите формы нужно валидировать все поля: ' +
+    '- имя и фамилия - без пробелов, с большой буквы, как минимум две буквы в слове; ' +
+    '- возраст - только положительное число, пусть будет от 18 до 100; ' +
+    '- электронный адрес - в формате name@xxx.com. ' +
+    'Невалидное поле должно подсвечиваться красным. Внизу формы показать красным цветом сообщение о невалидности полей, ' +
+    'перечислить эти поля (например, “Неправильно заполнены имя, возраст”).');
+
+  const form = document.querySelector('form#valid');
+  const inputs = form.querySelectorAll('input[type="text"]');
+  const text = form.querySelector('.before-submit');
+  const firstName = form.querySelector('input[name="firstname"]');
+  const secondName = form.querySelector('input[name="secondname"]');
+  const ageName = form.querySelector('input[name="age"]');
+  const emailName = form.querySelector('input[name="email"]');
+
+  const checkTextField = /^([А-Я]+[а-я]{2,})$/;
+  const checkEmailField = /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
+
+
+
+  form.addEventListener('submit', function(event) {
+    let i, value, name, input,
+      max = inputs.length,
+      formNonValid = 0,
+      errors = 'Неправильно заполнены:<ul>';
+
+
+      let firstname = firstName.value;
+      const validFirstName = checkTextField.test(firstname);
+      if (validFirstName) {
+        firstName.style.borderColor = '';
+        formNonValid++;
+      } else {
+        errors += ('<li>' + firstName.name + '</li>');
+        firstName.style.borderColor = 'red';
+      }
+
+      let secondname = secondName.value;
+      const validSecondName = checkTextField.test(secondname);
+      if (validSecondName) {
+        secondName.style.borderColor = '';
+        formNonValid++;
+      } else {
+        errors += ('<li>' + secondName.name + '</li>');
+        secondName.style.borderColor = 'red';
+      }
+
+      let age = ageName.value;
+      if (age > 18 && age < 100) {
+        ageName.style.borderColor = '';
+        formNonValid++;
+      } else {
+        errors += ('<li>' + ageName.name + '</li>');
+        ageName.style.borderColor = 'red';
+      }
+
+      let email = emailName.value;
+      const validEmail = checkEmailField.test(email);
+      if (validEmail) {
+        emailName.style.borderColor = '';
+        formNonValid++;
+      } else {
+        errors += ('<li>' + emailName.name + '</li>');
+        emailName.style.borderColor = 'red';
+      }
+
+    errors += '</ul>';
+    if (formNonValid < 4) {
+      event.preventDefault();
+      text.innerHTML = errors;
+    } else {
+      alert('Уважаемый ' + firstname + ' ' + secondname + '. Спасибо за регистрацию.')
+    }
+
+    console.log(errors);
+  });
+
+}());
+//
+(function(){
 
   console.log('2. Дан массив  [ {“лето”: “одень футболку и шорты”}, {“осень”: “одень куртку и штаны”}, ' +
     '{“зима”: “одень шубу и теплые штаны”}, {“весна”: “одень ветровку и джинсы”} ]. ' +
--- node/node.js
+++ node/node.js
@@ -25,40 +25,69 @@ function Node(element) {
   };
   this.innerHeight = function(html) {
     return parseInt(getComputedStyle(this.element).height);
-  }
+  };
   this.height = function(html) {
     return this.element.clientHeight;
-  }
+  };
   this.outherHeight = function(html) {
     return this.element.offsetHeight;
-  }
+  };
   this.innerWidth = function(html) {
     return parseInt(getComputedStyle(this.element).width);
-  }
+  };
   this.width = function(html) {
     return this.element.clientWidth;
-  }
+  };
   this.outherWidth = function(html) {
     return this.element.offsetWidth;
-  }
+  };
   this.style = function(html, property, value) {
     if (!value) {
       return getComputedStyle(this.element).getPropertyValue(property);
     } else {
       this.element.style.setProperty(property,value);
     }
-  }
+  };
   this.getPosition = function(html) {
     return {
       left: this.element.getBoundingClientRect().left,
       top: this.element.getBoundingClientRect().top
     }
-  }
+  };
   this.backToTop = function() {
     window.scrollTo(0, 0); // - метод backToTop() должен проскроллить страницу к её началу.
-  }
+  };
+  this.validateForm = function(form, fields) {
+    const hasErrors = form.querySelector('.errors');
+    if (hasErrors) {
+      hasErrors.remove();
+    }
 
+    const errorList = document.createElement('ul');
+    errorList.classList.add('errors');
+    const validateTypes = {
+      text: /[a-zа-яёA-ZА-ЯЁ]/,
+      digits: /\d/,
+      textDigits: /[a-zа-яё0-9]+/
+    };
 
+    fields.forEach(function(item) {
+      const fieldName = Object.keys(item)[0];
+      const type = item[fieldName];
+      const field = form.querySelector('[name="' + fieldName + '"]');
+      const isFieldValid = validateTypes[type].test(field.value);
+      if (!isFieldValid) {
+        field.classList.add('error');
+        const li = document.createElement('li');
+        li.textContent = 'Поле ' + fieldName + ' должно быть типа ' + type;
+        errorList.append(li);
+      } else {
+        field.classList.remove('error');
+      }
+    });
+    form.append(errorList);
+
+  };
 
   //this.event = function(eventName , callback) {
   //  if(!callback) {
@@ -78,30 +107,19 @@ function Node(element) {
 
 }
 
-const elem = document.querySelector('p');
-const uzel = new Node(elem);
-console.log(uzel.innerHeight());
-console.log(uzel.height());
-console.log(uzel.outherHeight());
-console.log(uzel.innerWidth());
-console.log(uzel.width());
-console.log(uzel.outherWidth());
-console.log(uzel.style(elem, 'color'));
-uzel.style(elem,'color','red');
-console.log(uzel.style(elem, 'color'));
-console.log(uzel.getPosition(elem));
-uzel.backToTop();
+const form1 = document.querySelector('#valid');
+const uzel = new Node();
+
+
+form1.addEventListener('submit', function(event) {
+  event.preventDefault();
+  uzel.validateForm(form1,[{firstname: 'text'}, {secondname: 'text'}, {age: 'digits'}, {login: 'textDigits'}]);
+
+});
+
+
 
 // 5. В nodes.js дополнить модуль nodes следующими методами:
 // - event(eventName [, callback]) - если передан один аргумент (имя события),
 // вызвать это событие; если передан и второй аргумент, добавить на событие eventName слушатель callback
 
-
-
-// 3. В nodes.js дополнить модуль nodes статическим методом validateForm, который примет первым аргументом форму,
-// а вторым - массив объектов вида  {inputName: testString} где inputName - имя поля, которое надо валидировать в форме,
-// а testString - одно из строковых значений:
-// - “text” - для этого значения поле должно содержать только буквы английского или русского алфавита;
-// - “digits” - только цифры; - “textDigits” - цифры и/или текст.
-// Например: nodes.validateForm(form1, [{name: ‘text’}, {age: ‘digits’}]) должен при сабмите формы проверить инпуты с именами name и age на наличие текста и числа.
-// Невалидные поля подсветить красным.  В форме показать сообщение о невалидности (сообщение добавлять средствами javascript в конец формы).
